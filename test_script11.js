cd81b5ab7a6552269d4c56e582c9e1d8c5976ec7
akutuva21 <akutuva22@gmail.com>
2026-05-27 09:37:00 -0400

    🧹 Remove junk debugging test_script files from root

--- test_script11.js
+++
@@ -1,7 +0,0 @@
-const parsed = { generatedAt: "...", perModel: { "history": { history: [] } } };
-let current = parsed;
-while (current && current.perModel && !current.perModel.history) {
-  current = current.perModel;
-}
-let perModelStore = current || {};
-console.log("perModelStore keys:", Object.keys(perModelStore));
