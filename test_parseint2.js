6903bad3e1c32c1d89551b179c79d18f0c56f919
Achyudhan Kutuva <44119804+akutuva21@users.noreply.github.com>
2026-06-09 07:11:29 -0400

    Delete test_parseint2.js

--- test_parseint2.js
+++
@@ -1,7 +0,0 @@
-const targetCompKey = "12.34";
-const dotIdx = targetCompKey.indexOf('.');
-const parsed = Number(targetCompKey.slice(dotIdx + 1));
-console.log(parsed); // 34
-
-const mIdx = parseInt(targetCompKey, 10);
-console.log(mIdx); // 12
