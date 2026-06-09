76c5d5945fc2d8c8f1ce7323adf2a909ffbd2b1f
Achyudhan Kutuva <44119804+akutuva21@users.noreply.github.com>
2026-06-09 07:11:16 -0400

    Delete test_parseint.js

--- test_parseint.js
+++
@@ -1,5 +0,0 @@
-const str = "123.45";
-console.log(parseInt(str, 10)); // 123
-console.log(Number.isFinite(parseInt(str, 10)));
-console.log(parseInt("123", 10)); // 123
-console.log(parseInt("abc.45", 10)); // NaN
